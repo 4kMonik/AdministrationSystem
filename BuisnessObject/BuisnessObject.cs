30dbb424760e1da3f13621fbeee1acf620aa14ab
4kMonik <f4kMonik@yandex.ru>
2023-06-06 21:49:24 +0300

    Добавьте файлы проекта.

---
+++ BuisnessObject/BuisnessObject.cs
@@ -0,0 +1,28 @@
+﻿namespace BuisnessObject
+{
+    public class userObject
+    {
+        public enum ROLES
+        {
+            user = 1,
+            admin,
+            guest
+        }
+        public int userId { get;}
+        public string userName { get; }
+        public ROLES userRole { get; }
+        public DateOnly userBirthDate { get; }
+
+        public List<DateTime> loginTime { get; set; }
+
+        public userObject(int id, string name, DateOnly birthDate, ROLES role)
+        {
+            this.userId = id;
+            this.userName = name;   
+            this.userRole = role;
+            this.userBirthDate = birthDate;
+        }
+
+
+    }
+}
